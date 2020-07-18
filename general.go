b5dcea6035225d35c0d363f029cde43dd89b9a76
Brent Bergwall <kothx@yahoo.com>
2020-07-18 13:34:29 -0500

    Add several new general interfaces

--- general.go
+++ general.go
@@ -23,6 +23,32 @@ type Logger interface {
 	Logf(format string, v ...interface{}) error
 }
 
+// LoggerFactory is a minimal interface to supports creating loggers
+type LoggerFactory interface {
+	New(name string) (Logger, error)
+}
+
+// Dumper is a minimal interface to support dumping large chunks of data
+type Dumper interface {
+	Dump(data []byte) error
+	DumpObj(obj interface{}) error
+}
+
+// DumperFactory is a minimal interface to supports creating dumpers
+type DumperFactory interface {
+	// New creates a new Dumper with the given name
+	// it may need to be closed, use "closer, ok := dumper.(io.Closer)" to find out
+	New(name string) (Dumper, error)
+
+	// Dump is a helper method that creates a new Dumper, dumps the one given buffer and closes
+	// the dumper
+	Dump(name string, data []byte) error
+
+	// DumpObj is a helper method that creates a new Dumper, dumps the one given object and closes
+	// the dumper
+	DumpObj(name string, obj interface{}) error
+}
+
 // Task represents a process that can be run and its inputs
 type Task interface {
 	// Name must return the name of the task with passible namespacing separated by slashes
@@ -30,4 +56,20 @@ type Task interface {
 	Name() string
 	Inputs() []string
 	Run(inputs ...string) error
+}
+
+// Config allows retreving configuration values
+type Config interface {
+	Value(section, name string) (string, error)
+}
+
+// PersistentState interface allows reading and writing named state objects
+type PersistentState interface {
+	// Save persists an object under the given name
+	// the state object should be json serializable
+	Save(name string, state interface{}) error
+
+	// Retrieve fills the provided state object with data that was previously saved under the
+	// given name.
+	Retrieve(name string, state interface{}) error
 }
